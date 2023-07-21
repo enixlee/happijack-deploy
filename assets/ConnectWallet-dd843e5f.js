16ed0abb3b5316138cde8ed044dcf927265f7bd4
enixlee <405284695@qq.com>
2023-07-21 08:29:53 +0800

    deploy

---
+++ assets/ConnectWallet-dd843e5f.js
@@ -0,0 +1,1 @@
+import{Z as c,l as r,n as i,o as n,bx as a,s as e,by as u}from"./index-a6d93485.js";function f(){const s=c(),o=r(),t=()=>s("/");return i.useEffect(()=>(n.instance.on(a.event,t),()=>{n.instance.off(a.event,t)}),[o.pathname]),e.jsx(e.Fragment,{children:e.jsx(u,{})})}export{f as default};
