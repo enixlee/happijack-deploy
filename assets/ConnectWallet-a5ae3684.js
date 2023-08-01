fded621d785a2038de7528ddb4eefb68fdf3497c
enixlee <405284695@qq.com>
2023-08-01 21:46:58 +0800

    deploy

---
+++ assets/ConnectWallet-a5ae3684.js
@@ -0,0 +1,1 @@
+import{a0 as c,l as r,n as i,bA as l,o as a,bB as n,s as e,bC as u}from"./index-fcdc715d.js";function m(){const s=c(),o=r(),t=()=>s("/");return i.useEffect(()=>(l.instance.setUser(null),a.instance.on(n.event,t),()=>{a.instance.off(n.event,t)}),[o.pathname]),e.jsx(e.Fragment,{children:e.jsx(u,{})})}export{m as default};
--- assets/NotFound-5ce126f9.js
+++
@@ -1,1 +0,0 @@
-import{s as e,S as t,C as n}from"./index-41ba7acc.js";function i(){return e.jsx(e.Fragment,{children:e.jsx(t,{direction:"vertical",style:{display:"grid",justifyContent:"center",alignContent:"center",height:`${n}`},children:e.jsx("label",{style:{fontSize:30,color:"#52c41a"},children:"Page Not Found."})})})}export{i as default};
